516a05e57d12bce9932fb07916fab7acd3c67bc6
Gilbert Zaini <gilbertzaini03@gmail.com>
2024-06-10 11:23:06 +0700

    add load model test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import { ChakraProvider } from '@chakra-ui/react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 import Dashboard from './Page/Dashboard';
+import ModelTest from './Page/ModelTest';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Dashboard />} />
+          <Route path="/model-test" element={<ModelTest />} />
         </Routes>
       </BrowserRouter>
     </ChakraProvider>
