70eb3282b311cbdc11425cc82f1df1866039173e
Gilbert Zaini <gilbertzaini03@gmail.com>
2024-06-12 17:36:44 +0700

    add profile page

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -59,7 +59,7 @@ const Navbar = () => {
             <RiHistoryLine fill="grey" />
           )}
         </Tab>
-        <Tab>
+        <Tab onClick={() => Page.setPage("Camera")}>
           <IoScanCircleSharp size={"40px"} />
         </Tab>
         <Tab onClick={Page.hama}>
@@ -76,8 +76,8 @@ const Navbar = () => {
             <MdOutlineArticle fill="grey" />
           )}
         </Tab>
-        <Tab onClick={Page.informasi}>
-          {Page.page === "Informasi Hama" ? (
+        <Tab onClick={() => {Page.setPage("Profil")}}>
+          {Page.page === "Profil" ? (
             <Box
               display={"flex"}
               flexDirection={"column"}
