81cb25fc62aea2df1ee09ae8cc423c4afce22b96
Gilbert Zaini <gilbertzaini03@gmail.com>
2024-06-12 15:41:47 +0700

    add ManajemenHamaPage

--- src/Page/ManajemenHamaPage.jsx
+++ src/Page/ManajemenHamaPage.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import React from "react";
+import ManajemenHamaCard from "../Components/ManajemenHamaCard";
+import { Flex, Text } from "@chakra-ui/react";
 
 const ManajemenHamaPage = () => {
-  return (
-    <div>ManajemenHamaPage</div>
-  )
-}
+  const hamaDetails = [
+    {
+      date: "2024-5-20",
+      jenis: "Lalat Buah",
+      jumlah: 400,
+    },
+    {
+      date: "2024-4-20",
+      jenis: "Lalat Buah",
+      jumlah: 200,
+    },
+    {
+      date: "2024-3-20",
+      jenis: "Lalat Buah",
+      jumlah: 300,
+    },
+  ];
 
-export default ManajemenHamaPage
+  return (
+    <Flex direction={"column"} w={"85%"} mx={"auto"} pos={"relative"}>
+      <Flex pos={"relative"} mt={"7vh"} w={"100%"} mb={18} textAlign={"start"}>
+        <Text
+          fontWeight={"bold"}
+          w={"100%"}
+          fontSize={"2.3vh"}
+          color={"#2c3631"}
+        >
+          Manajemen Hama{" "}
+        </Text>
+        <Flex
+          justify={"center"}
+          align={"center"}
+          w={"30px"}
+          h={"30px"}
+          bg={"#fcfcfc"}
+          borderRadius={"50%"}
+          p={2}
+        >
+          <Text>?</Text>
+        </Flex>
+      </Flex>
+
+      <Flex direction={"column"} gap={5}>
+        {hamaDetails.map((item) => (
+          <ManajemenHamaCard item={item} />
+        ))}
+      </Flex>
+    </Flex>
+  );
+};
+
+export default ManajemenHamaPage;
