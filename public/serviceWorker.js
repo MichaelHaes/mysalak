191ff57f593b752a717dc2641d4e0f82a8d37cd1
Gilbert Zaini <gilbertzaini03@gmail.com>
2024-07-23 13:42:49 +0700

    fix pwa

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,63 +1,42 @@
 const CACHE_NAME = "version-1";
 const urlsToCache = ["index.html", "offline.html"];
 
+const self = this;
+
 // Install SW
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches
-      .open(CACHE_NAME)
-      .then((cache) => {
-        console.log("Opened cache");
-        return cache.addAll(urlsToCache);
-      })
-      .catch((error) => {
-        console.error("Cache open failed: ", error);
-      })
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Opened cache");
+
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
 // Listen for requests
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches
-      .match(event.request)
-      .then((response) => {
-        // Return cached response if found, otherwise fetch from network
-        console.log("cache match");
-        return (
-          response ||
-          fetch(event.request).catch(() => {
-            return caches.match("offline.html");
-          })
-        );
-      })
-      .catch((error) => {
-        console.error("Fetch failed: ", error);
-        return caches.match("offline.html");
-      })
+    caches.match(event.request).then(() => {
+      return fetch(event.request).catch(() => caches.match("offline.html"));
+    })
   );
 });
 
 // Activate the SW
 self.addEventListener("activate", (event) => {
-  const cacheWhitelist = [CACHE_NAME];
+  const cacheWhitelist = [];
+  cacheWhitelist.push(CACHE_NAME);
 
   event.waitUntil(
-    caches
-      .keys()
-      .then((cacheNames) => {
-        console.log("activated");
-        return Promise.all(
-          cacheNames.map((cacheName) => {
-            if (!cacheWhitelist.includes(cacheName)) {
-              console.log("Deleting cache: ", cacheName);
-              return caches.delete(cacheName);
-            }
-          })
-        );
-      })
-      .catch((error) => {
-        console.error("Activation failed: ", error);
-      })
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    )
   );
 });
