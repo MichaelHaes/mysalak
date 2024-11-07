2eff51f8fa6462953bfe928eab6c0bb0c934fc13
RichardTandean <115069181+RichardTandean@users.noreply.github.com>
2024-11-07 14:31:38 +0700

    add logout button

--- src/Page/Dashboard.jsx
+++ src/Page/Dashboard.jsx
@@ -3,8 +3,19 @@ import Weather from "../Components/Weather";
 import ManajemenHama from "../Components/ManajemenHama";
 import DashboardHeader from "../Components/DashboardHeader";
 import {requestForToken} from "../firebaseNotification/firebase";
+import {useNavigate} from "react-router-dom";
+import axios from "axios";
+import env from "react-dotenv";
+import toast from "react-hot-toast";
 
 const Dashboard = () => {
+  const navigate = useNavigate();
+  const showToast = (response) =>
+    toast(
+      <div>
+        <p>{response}</p>
+      </div>
+    );
 
   useEffect(() => {
     if(!localStorage.getItem('fcmToken')){
@@ -12,11 +23,28 @@ const Dashboard = () => {
     }
   }, []);
 
+  const handleLogOut = () => {
+    localStorage.removeItem("JWT_Token");
+    localStorage.removeItem("fcmToken");
+    localStorage.removeItem("role_id");
+    localStorage.removeItem("user_id");
+    axios.post(`${env.API_URL}/mysalak/delete-token`, { device_id: localStorage.getItem("device_id") })
+      .then((response) => {
+        showToast("Logout Success")
+        navigate("/")
+      })
+  }
+
   return (
     <div>
       <DashboardHeader />
       <Weather />
       <ManajemenHama />
+      <button
+        onClick={handleLogOut}
+      >
+        Logout
+      </button>
     </div>
   );
 };
