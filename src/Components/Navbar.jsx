dac66ad2c9935e79e3a30b6cc235a8749860b1f2
Gilbert Zaini <gilbertzaini03@gmail.com>
2024-12-24 14:35:35 +0700

    ref: navbar now has role-based menu

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Tabs, TabList, Tab, Box, Flex } from "@chakra-ui/react";
 import { GoHomeFill } from "react-icons/go";
 import { useHama } from "../state";
 import { IoBug, IoScan } from "react-icons/io5";
-import { MdArticle } from "react-icons/md";
+import { MdArticle, MdOutlineVerified } from "react-icons/md";
 import { FaMapMarkedAlt } from "react-icons/fa";
 import { useLocation, useNavigate } from "react-router-dom";
+import { BsFillPeopleFill } from "react-icons/bs";
 
 const Navbar = () => {
   const { resetDetail } = useHama();
   const navigate = useNavigate();
   const url = useLocation().pathname;
+  const roleId = localStorage.getItem("role_id");
 
   const navItems = [
     {
@@ -21,11 +23,26 @@ const Navbar = () => {
       route: "/persebaran-hama",
       a_logo: <FaMapMarkedAlt size={"auto"} />,
     },
+    // role based menu
     {
       route: "/kamera",
       a_logo: <IoScan size={"auto"} />,
       custom: true,
+      role: 5,
     },
+    {
+      route: "/admin/manajemen-anggota",
+      a_logo: <BsFillPeopleFill size={"auto"} />,
+      custom: true,
+      role: 1,
+    },
+    {
+      route: "/admin/verifikasi-anggota",
+      a_logo: <MdOutlineVerified size={"auto"} />,
+      custom: true,
+      role: 3,
+    },
+    //
     {
       route: "/manajemen-hama",
       a_logo: <IoBug size={"auto"} />,
@@ -66,45 +83,47 @@ const Navbar = () => {
         paddingX={"5%"}
         w={"100%"}
       >
-        {navItems.map((item, index) => (
-          <Tab
-            key={index}
-            onClick={() => {
-              navigate(item.route);
-              resetDetail();
-            }}
-            w={item.custom ? "25%" : "15.5%"}
-          >
-            <Flex
-              flexDirection={"column"}
-              alignItems={"center"}
-              justify={"center"}
-              borderRadius={"full"}
-              h={"100%"}
-              w={"100%"}
-              bg={
-                item.custom
-                  ? "linear-gradient(to top, #6BBA5E, #2C3631)"
-                  : "transparent"
-              }
-              p={item.custom ? 2 : 0}
-              border={item.custom ? "5px solid rgba(231,231,231,1)" : "0px"}
-              color={
-                item.custom ? "white" : url === item.route ? "black" : "gray"
-              }
-              mt={item.custom ? -7 : 1}
+        {navItems
+          .filter((item) => !item.custom || item.role === parseInt(roleId))
+          .map((item, index) => (
+            <Tab
+              key={index}
+              onClick={() => {
+                navigate(item.route);
+                resetDetail();
+              }}
+              w={item.custom ? "25%" : "15.5%"}
             >
-              {url === item.route ? (
-                <>
-                  {item.a_logo}
-                  {!item.custom && <IconActive />}
-                </>
-              ) : (
-                <>{item.a_logo}</>
-              )}
-            </Flex>
-          </Tab>
-        ))}
+              <Flex
+                flexDirection={"column"}
+                alignItems={"center"}
+                justify={"center"}
+                borderRadius={"full"}
+                h={"100%"}
+                w={"100%"}
+                bg={
+                  item.custom
+                    ? "linear-gradient(to top, #6BBA5E, #2C3631)"
+                    : "transparent"
+                }
+                p={item.custom ? 2 : 0}
+                border={item.custom ? "5px solid rgba(231,231,231,1)" : "0px"}
+                color={
+                  item.custom ? "white" : url === item.route ? "black" : "gray"
+                }
+                mt={item.custom ? -7 : 1}
+              >
+                {url === item.route ? (
+                  <>
+                    {item.a_logo}
+                    {!item.custom && <IconActive />}
+                  </>
+                ) : (
+                  <>{item.a_logo}</>
+                )}
+              </Flex>
+            </Tab>
+          ))}
       </TabList>
     </Tabs>
   );
