caf11cf3332608a8762608fc655055580c063089
Gilbert Zaini <gilbertzaini03@gmail.com>
2025-01-02 09:49:52 +0700

    removed unused pages

--- src/Page/HitungHama.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const HitungHama = () => {
-  return (
-    <div>HitungHama</div>
-  )
-}
-
-export default HitungHama
--- src/Page/ManajemenHamaDetail.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ManajemenHamaDetail = () => {
-  return (
-    <div>ManajemenHamaDetail</div>
-  )
-}
-
-export default ManajemenHamaDetail
